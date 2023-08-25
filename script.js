42076756058e2b62bba9c8d850b7f76ff848f928
EmanuelW22 <emerticariu22@gmail.com>
2023-08-25 18:08:41 +0300

    update.html

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
 const sentences = [
   "The beautiful sunset painted the sky in hues of orange, pink, and gold, creating a breathtaking scene, As the waves gently lapped the shore, the seagulls soared overhead, their cries mingling with the sound of the ocean.",
-  "The bustling city streets were alive with the rhythm of life, as people hurried to their destinations and cars honked in the distance,Amidst the lush greenery of the forest, a winding path led to a hidden waterfall, its cascading waters creating a soothing melody.",
+  "The bustling city streets were alive with the rhythm of life, as people hurried to their destinations and cars honked in the distance, Amidst the lush greenery of the forest, a winding path led to a hidden waterfall, its cascading waters creating a soothing melody.",
   "The aroma of freshly baked bread wafted through the air, enticing passersby to step into the quaint bakery on the corner, with a canvas in hand, the artist began to translate the beauty of nature onto the blank surface, strokes of color bringing the scene to life.",
 ];
 
 let buttonStart = document.getElementById("start");
-let conntent = document.getElementById("quote");
+let content = document.getElementById("quote");
 let result = document.getElementById("result");
 let inputText = document.getElementById("text-input");
-let wrong = document.getElementById("wrong")
+let timer = document.getElementById("timer");
 
 let interval;
 let seconds = 0;
 
-buttonStart.addEventListener("click", function () {
+function startGame() {
   if (seconds >= 60) return;
   let randomIndex = Math.floor(Math.random() * sentences.length);
-  conntent.textContent = sentences[randomIndex];
+  content.textContent = sentences[randomIndex];
   seconds = 0;
   inputText.value = "";
   inputText.disabled = false;
   startTimer();
-});
+}
 
-inputText.addEventListener("input", function (event) {
+function textInput() {
   if (seconds >= 60) {
-    event.target.value = "";
+    inputText.value = "";
     return;
   }
-  let typedText = event.target.value;
+
+  let typedText = inputText.value;
   let coloredContent = "";
 
-  for (let i = 0; i < conntent.textContent.length; ++i) {
+  for (let i = 0; i < content.textContent.length; ++i) {
     if (i < typedText.length) {
-      if (typedText[i] === conntent.textContent[i]) {
-        coloredContent += `<span style="color: green;">${conntent.textContent[i]}</span>`;
+      if (typedText[i] === content.textContent[i]) {
+        coloredContent += `<span style="color: green;">${content.textContent[i]}</span>`;
       } else {
-        coloredContent += `<span style="color: red;">${conntent.textContent[i]}</span>`;
+        coloredContent += `<span style="color: red;">${content.textContent[i]}</span>`;
       }
     } else {
-      coloredContent += conntent.textContent[i];
+      coloredContent += content.textContent[i];
     }
   }
-  conntent.innerHTML = coloredContent;
+  content.innerHTML = coloredContent;
 
   if (seconds >= 60) {
     inputText.disabled = true;
   }
 
   countCorrectWords();
-});
+}
 
 function startTimer() {
   clearInterval(interval);
@@ -68,15 +69,13 @@ function startTimer() {
 function countCorrectWords() {
   let typedText = inputText.value.trim();
   let typedWords = typedText.split(/\s+/);
-  let contentWords = conntent.textContent.trim().split(/\s+/);
+  let contentWords = content.textContent.trim().split(/\s+/);
   let correctWordsCount = 0;
 
   for (let i = 0; i < contentWords.length; ++i) {
     if (i < typedWords.length && typedWords[i] === contentWords[i]) {
       ++correctWordsCount;
-    } 
+    }
   }
-
   result.textContent = correctWordsCount;
-
 }
